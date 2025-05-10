f752445d7c28927505a6f2f54a55342aea0816c3
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-05-10 16:45:37 +0100

    Improve formatting of dates in output

--- src/modmail.ts
+++ src/modmail.ts
@@ -3,6 +3,11 @@ import { ModMail } from "@devvit/protos";
 import { parseCancellation, parseCommandDate } from "./commandParser.js";
 import { cancelReminder, getConversationReminderDate, queueReminder } from "./sendReminders.js";
 import json2md from "json2md";
+import { format } from "date-fns";
+
+export function formatDate (date: Date) {
+    return format(date, "EEEE, MMM d, yyyy 'at' HH:mm 'UTC'");
+}
 
 export async function handleModmail (event: ModMail, context: TriggerContext) {
     if (event.messageAuthor?.name === context.appName) {
@@ -41,18 +46,18 @@ export async function handleModmail (event: ModMail, context: TriggerContext) {
             message.push({ p: "No reminder is scheduled for this modmail conversation." });
         } else {
             await cancelReminder(event.conversationId, context);
-            message.push({ p: `Reminder for this modmail conversation has been cancelled. The reminder was scheduled for ${reminderDate.toUTCString()}.` });
+            message.push({ p: `Reminder for this modmail conversation has been cancelled. The reminder was scheduled for ${formatDate(reminderDate)}.` });
         }
     };
 
     const reminderDate = parseCommandDate(currentMessage.bodyMarkdown);
     if (reminderDate) {
-        console.log(`Modmail: Remind command found in message ${currentMessage.id} for ${reminderDate.toUTCString()}`);
+        console.log(`Modmail: Remind command found in message ${currentMessage.id} for ${formatDate(reminderDate)}`);
         const existingReminderDate = await getConversationReminderDate(event.conversationId, context);
         await queueReminder(event.conversationId, currentMessage.author?.name, reminderDate, context);
-        message.push({ p: `A reminder for this modmail conversation has been scheduled for ${reminderDate.toUTCString()}.` });
+        message.push({ p: `A reminder for this modmail conversation has been scheduled for ${formatDate(reminderDate)}.` });
         if (existingReminderDate) {
-            message.push({ p: `A previous reminder was scheduled for ${existingReminderDate.toUTCString()}, and this has been replaced.` });
+            message.push({ p: `A previous reminder was scheduled for ${formatDate(existingReminderDate)}, and this has been replaced.` });
         }
     }
 
@@ -62,5 +67,7 @@ export async function handleModmail (event: ModMail, context: TriggerContext) {
             body: json2md(message),
             isInternal: true,
         });
+    } else {
+        console.log(`Modmail: No RemindMe! command in message ${currentMessage.id}`);
     }
 }
