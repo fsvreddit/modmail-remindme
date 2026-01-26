957cb1181cc8ddc085679e9b7ff48aaef912246b
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-01-26 07:55:09 +0000

    Refactor onAppUpgrade handler to remove unused request logging

--- src/server/triggers/onAppUpgrade.ts
+++ src/server/triggers/onAppUpgrade.ts
@@ -1,12 +1,7 @@
-import { OnAppUpgradeRequest } from "@devvit/web/shared";
 import { Request, Response } from "express";
 import { queueAdhocTask } from "../core";
 
-export const onAppUpgrade = async (request: Request, response: Response) => {
-    console.log("onAppUpgrade:", request.body);
-    const event = request.body as OnAppUpgradeRequest;
-    console.log(event);
-
+export const onAppUpgrade = async (_: Request, response: Response) => {
     await queueAdhocTask();
 
     return response.status(200).json({ message: "app upgraded" });
